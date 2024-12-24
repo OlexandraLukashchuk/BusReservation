f34df30e38ea7d645ac1273d5938927a11e91a9b
OlexandraLukashchuk <olexandralukashchyk@gmail.com>
2024-12-24 01:22:20 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/AutobusApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutobusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutobusApplication.class, args);
+	}
+
+}
